2229f79b5ff27d151d3ab98439efda96aded10a0
prajapativ107 <prajapativ107@gmail.com>
2025-07-24 14:29:04 +0530

    test-block

---
+++ wp-content/plugins/test-block/build/test-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bd919c4160fac967f8e3');
---
+++ wp-content/plugins/test-block/build/test-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '447b8da7f682eb43b25d');
